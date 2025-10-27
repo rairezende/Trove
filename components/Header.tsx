e8ab42b008ac8d3a49664f725c3c2b9cee40f36e
Raiane <raiane.rze@gmail.com>
2025-10-27 18:15:34 -0300

    adds img logo to navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ListPlus, PlusCircle } from 'lucide-react';
+import {Logo} from '../assets/index.ts'
 
 const Header: React.FC = () => {
     const navigate = useNavigate();
     return (
-        <header className="bg-white shadow-md sticky top-0 z-10">
+        <header className="bg-tertiary shadow-md sticky top-0 z-10">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
                 <Link to="/" className="flex items-center gap-2 text-2xl font-bold text-stone-700 hover:text-secondary transition-colors">
                     <ListPlus className="w-8 h-8 text-primary" />
-                    <span>Trovy</span>
+                    <img src={Logo} style={{height: 33}} alt="Trovy logo is the name in a light orange color with an elephant as the 'O'" />
                 </Link>
                 <button
                     onClick={() => navigate('/create')}
